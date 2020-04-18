9c50b9033950d8c61ed849caba02ada0e7c20c29
lit <tt137378245@outlook.com>
2020-04-18 14:10:09 +0800

    feat: 完成数据报表

---
+++ src/components/data/Data.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+      <div class="text item">
+        <div id="main" style="width: 600px;height:400px;"></div>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import _ from 'lodash'
+
+export default {
+  data: function () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  mounted () {
+    // 页面上的DOM已经渲染完毕后进行图表初始化
+    var myChart = echarts.init(document.getElementById('main'))
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: '用户来源'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+    const datas = {}
+    // 使用lodash合并对象
+    const result = _.merge(datas, this.options)
+    console.log(result)
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
